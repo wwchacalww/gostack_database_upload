243ba84f31a2590dbcd256fa0524211f2e2577e8
Vicente Cartaxo <juniorcartaxo@gmail.com>
2020-11-07 13:11:53 -0300

    Create transaction and category is done.

---
+++ src/database/migrations/1604755709053-CreateTransactionsTable.ts
@@ -0,0 +1,72 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateTransactionsTable1604755709053
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'NUMERIC(7,2)',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('TransactionCategory', 'transactions');
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
+import { getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    let category_id: string;
+    const categoryRepository = getRepository(Category);
+
+    const categoryInDB = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryInDB) {
+      const categoryNew = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryNew);
+      category_id = categoryNew.id;
+    } else {
+      category_id = categoryInDB.id;
+    }
+
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
